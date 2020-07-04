ed809a12c3bf8fab5b1d668c252adafaaf797464
Evgen-ios <jack38irk@icloud.com>
2020-07-04 22:45:02 +0800

    Add Map Controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Евгений on 04.07.2020.
+//  Copyright © 2020 Evgen Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
