ee002524ddc8e6d2e28a5a4090a23b3b728a17af
Evgen-ios <jack38irk@icloud.com>
2020-07-12 12:11:42 +0800

    Add anotation View in Map

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -11,11 +11,14 @@ import MapKit
 class MapViewController: UIViewController {
     
     var place : Place!
+    let annotationIdentifer = "annotationIdentifer"
+    
     
     @IBOutlet var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         setupPlacemark()
 
     }
@@ -52,4 +55,29 @@ class MapViewController: UIViewController {
         }
     }
     
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard  !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView( withIdentifier: annotationIdentifer ) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifer)
+            annotationView?.canShowCallout = true
+        }
+        if let imageDate = place.imageData {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(data: imageDate)
+            annotationView?.rightCalloutAccessoryView = imageView
+            
+        }
+        
+        return annotationView
+        
+    }
 }
